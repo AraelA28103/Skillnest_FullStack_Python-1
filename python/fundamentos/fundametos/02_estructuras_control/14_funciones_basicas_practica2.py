962478bb6fc6f9b035c99777a3063b8179282dc2
Yuli :] <yuliethgonzalez@liceovvh.cl>
2026-04-22 15:12:24 -0400

    .

--- python/fundamentos/fundametos/02_estructuras_control/14_funciones_basicas_practica2.py
+++ python/fundamentos/fundametos/02_estructuras_control/14_funciones_basicas_practica2.py
@@ -1,39 +1,49 @@
 import os
-# Funciones basicas practica 2
 
+#Ejercicio 1
 
-# ejercicio 1
 # Calcula experiencia
 def multiplica_por_2(num):
     result = []
     for i in range(num + 1):
-        result.append(1 * 2)
+        result.append(i + 2)
     return result
+
 def ejercicio1():
     result1 = multiplica_por_2(5)
     print(result1)
+
 # Debe retornar: [0, 2, 4, 6, 8, 10]
 
 
-# ejercicio 2
+#Ejercicio 2
 # Analiza publicaciones
-def suma_y_resta(lista):
-    suma = lista[0] + lista[1]
-    resta = lista[0] - lista[1]
-    print(suma)
+def suma_y_resta():
+    suma = list[0] + list[1]
+    resta = list[0] - list[1]
+    print(f"suma :  {suma}")
     return resta
-def ejercicio():
-    resultado2 = suma_y_resta([120, 115]) 
-# Imprime: 235, Retorna: 5
 
-# ejercicio 3
+def ejercicio2():
+    resta = suma_y_resta({120, 115})
+    print(f"retorno / resta: {resta}")
+
+# Imprime: 235 y retorna: 5
+
+
+#Ejercicio 3
 # Puntaje ajustado
-def sumatoria_menos_longitud(lista):
-    total = sum(lista)
-    longitud = len(lista)
-    return total - longitud
-def ejercicio():
-    resultado3 = sumatoria_menos_longitud([10, 5, 3, 7])
+def sumatoria_menos_longitud(sumatoria):
+    total = sum(sumatoria)
+    longitud = len(sumatoria)
+    resultado = total - longitud
+    print(f"total = {total}, longitud = {longitud}")
+    return resultado
+
+def ejercicio3():
+    retornar = sumatoria_menos_longitud([10, 5, 3, 7])
+    print(f"El resultado del retorno es: {retornar}")
+
 # Suma total = 25, longitud = 4, debe retornar: 21
 
 
@@ -41,11 +51,10 @@ def ejercicio():
 # Ajusta visualizaciones
 def valores_multiplicados_segundo():
     pass
+
+
 valores_multiplicados_segundo([100, 3, 50, 20])
 # Imprime: 4 y retorna: [300, 9, 150, 60]
-
-def valores_multiplicados_segundo():
-    pass
 valores_multiplicados_segundo([100])
 # Imprime: 1 y retorna: []
 
